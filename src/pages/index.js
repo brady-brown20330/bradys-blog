5699880a4f6c29c614f06b4689ddccc69cd9fbee
brownb202 <brownb202@gmail.com>
2020-12-21 18:16:40 -0700

    Added link paths

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,52 @@
 import React from 'react';
+import { graphql, Link } from 'gatsby';
 import { useSiteMetadata } from '../hooks/useSiteMetadata';
 import styled from 'styled-components';
 import { Layout } from '../components/Layout';
+import Dump from '../components/Dump';
 
-export default () => {
+const IndexWrapper = styled.main``;
+
+const PostWrapper = styled.div``;
+
+export default ({data}) => {
+  console.log(data)
   return (
-    <>
-    <Layout />
-    </>
+    <Layout>
+    <IndexWrapper>
+      {data.allMdx.nodes.map(
+        ({ id, excerpt, frontmatter, fields }) => (
+          <PostWrapper key={id}>
+            <Link to={fields.slug}>
+              <h1>{frontmatter.title}</h1>
+              <p>{frontmatter.date}</p>
+              <p>{excerpt}</p>
+            </Link>
+          </PostWrapper>
+        )
+      )}
+    </IndexWrapper>
+  </Layout>
   );
 };
+
+export const query = graphql`
+  query SITE_INDEX_QUERY {
+    allMdx(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { published: { eq: true } } }
+    ) {
+      nodes {
+        id
+        excerpt(pruneLength: 250)
+        frontmatter {
+          title
+          date
+        }
+        fields {
+          slug
+        }
+      }
+    }
+  }
+`;
